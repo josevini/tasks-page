3b554123b59cd772d8f4c9b68ad75dcd61fbae0e
Vinicius Silva <ferreiravini468@gmail.com>
2022-06-24 01:52:13 -0300

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import api from './api/api'
+
 export default function App() {
+
+  const [newTask, setNewTask] = useState([])
+  const [allTasks, setAllTasks] = useState([])
+
+  useEffect(() => {
+    async function getAll() {
+      const response = await api.get('/task/all')
+      setAllTasks(response.data)
+    }
+    getAll()
+  }, [])
+
   return (
-    <h1>React App</h1>
+    <>
+      <div className='container'>
+        <input className='form'
+          onBlur={(event) => {
+            setNewTask(event.target.value)
+          }} name='title' placeholder='Nova tarefa' />
+
+        <button className='btn'
+          onClick={() => {
+            api.post('/task/add', {
+              title: newTask
+            })
+            
+          }}
+        >
+          Cadastrar
+        </button>
+
+        {allTasks.map((value, key) => (
+          <div key={key} id={value.id} className='task'>
+            <p>{value.title}</p>
+          </div>    
+        ))}
+      </div>
+    </>
   )
 }
